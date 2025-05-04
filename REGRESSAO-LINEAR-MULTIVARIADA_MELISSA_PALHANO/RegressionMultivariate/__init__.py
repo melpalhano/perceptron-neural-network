a48612ef984ba6d95cd5d471d89a115e4e6627ed
Melissa Palhano <mlssplhn@gmail.com>
2025-05-04 17:18:53 -0300

    feat: configure Conda environment

---
+++ REGRESSAO-LINEAR-MULTIVARIADA_MELISSA_PALHANO/RegressionMultivariate/__init__.py
@@ -0,0 +1,21 @@
+"""
+RegressionMultivariate - Pacote para implementação de regressão linear multivariada
+"""
+
+__version__ = '1.0.0'
+__author__ = 'Melissa Rodrigues Palhano'
+
+from .features_normalize import features_normalize_by_std, features_normalizes_by_min_max
+from .compute_cost_multi import compute_cost_multi
+from .gradient_descent_multi import gradient_descent_multi
+from .gradient_descent_multi_with_history import gradient_descent_multi_with_history
+from .normal_eqn import normal_eqn
+
+__all__ = [
+    'features_normalize_by_std',
+    'features_normalizes_by_min_max',
+    'compute_cost_multi',
+    'gradient_descent_multi',
+    'gradient_descent_multi_with_history',
+    'normal_eqn'
+] 
